8035c1868472a8cebb20b3970205d9bd7581987f
mario chulian <mario.chulian@iesdonana.org>
2024-05-28 17:50:43 +0200

    menu

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Restaurant Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <style>
+    .background {
+      background: radial-gradient(circle at top, #111, #000); /* Establece el fondo negro */
+      color: white; /* Cambia el color del texto para que sea legible en negro */
+      min-height: 100vh; /* Asegura que el fondo negro ocupe todo el viewport */
+      font-family: sans-serif; /* Agrega una fuente predeterminada para el texto */
+    }
+
+  
+
+  </style>
+</head>
+<body class="background text-white font-sans">
+ hola
+</body>
+</html>
